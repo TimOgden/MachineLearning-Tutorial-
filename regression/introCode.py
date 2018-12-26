182bf48489fa1afaaa825cb0b0176c830f507835
Tim Ogden <togden1999@gmail.com>
2018-12-26 18:37:47 -0500

    Cleaned data

--- regression/introCode.py
+++ regression/introCode.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -13,4 +14,13 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] * 100
 
 #Now let's overwrite our dataframe with a new dataframe of only what we care about
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+forecast_col = 'Adj. Close'
+
+df.fillna(-999999, inplace=True)
+
+#This is the amount of days out we will predict using this data
+forecast_out = int(math.ceil(0.01*len(df)))
+#Need to shift the column up by the forecast_out amount because it is a future prediction
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 print(df.head())
