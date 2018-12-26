19ffc8495f2da1c11955cb950ffcbedab802a818
Tim Ogden <togden1999@gmail.com>
2018-12-26 18:48:20 -0500

    Scaling features

--- regression/introCode.py
+++ regression/introCode.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import quandl
 import math
-
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import numpy as np
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 
@@ -24,3 +26,9 @@ forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 print(df.head())
+
+#1 means drop the column, not the row
+X = np.array(df.drop(['label']), 1)
+y = np.array(df['label'])
+#Scikitlearn really helps in scaling
+X = preprocessing.scale(X)
