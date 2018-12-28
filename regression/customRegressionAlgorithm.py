5ccdd25900ff951cf540f3e73c994a124ab547c4
Tim Ogden <togden1999@gmail.com>
2018-12-27 19:18:53 -0500

    My own function for coefficient of correlation

--- regression/customRegressionAlgorithm.py
+++ regression/customRegressionAlgorithm.py
@@ -19,4 +19,22 @@ def best_fit_slope_intercept(xs, ys):
 	#return m
 
 m, b = best_fit_slope_intercept(xs, ys)
-print(m, b)
+
+regression_line = [(m*x) + b for x in xs]
+#plt.scatter(xs, ys)
+#plt.plot(xs, regression_line)
+#plt.show()
+
+#Let's try to calculate r^2 before watching the tutorial for it
+def coefficient_of_correlation(xs, ys, regression_line):
+	#Calculate the squared error of the regression line
+	se_regression = 0
+	for c, y_hat in enumerate(regression_line):
+		se_regression += (y_hat - ys[c])**2
+	#Calculate the squared error of the mean of the y's
+	se_mean_ys = 0
+	for c in ys:
+		se_mean_ys += (c - mean(ys))**2
+	return 1 - (se_regression/se_mean_ys)
+
+print(coefficient_of_correlation(xs,ys,regression_line))
