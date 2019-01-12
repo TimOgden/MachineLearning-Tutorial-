627b967d512bf50210079a523748901417ab7b16
Tim Ogden <togden1999@gmail.com>
2019-01-12 04:30:03 -0500

    Open AI Neural Net

--- deeplearning/openai.py
+++ deeplearning/openai.py
@@ -2,7 +2,7 @@ import gym
 import random
 import numpy as np
 import keras
-from keras.layers import Dense, Dropout, Input
+from keras.layers import Dense, Dropout, Flatten
 from statistics import mean, median
 from collections import Counter
 
@@ -46,11 +46,12 @@ def initial_population():
 		if score >= score_requirement:
 			accepted_scores.append(score)
 			for data in game_memory:
-				if data[1] == 1:
-					output = [0,1]
-				elif data[1] == 0:
-					output = [1,0]
-				training_data.append([data[0], output])
+				#if data[1] == 1:
+				#	output = [0,1]
+				#elif data[1] == 0:
+				#	output = [1,0]
+				#training_data.append([data[0], output])
+				training_data.append([data[0], data[1]])
 		env.reset()
 		scores.append(score)
 	training_data_save = np.array(training_data)
@@ -65,7 +66,7 @@ def initial_population():
 def neural_network_model(input_size):
 	model = keras.Sequential()
 	#model.add(Input(shape=(None,input_size,1)))
-	model.add(Dense(128, activation='relu', input_shape=(None,input_size,1)))
+	model.add(Dense(128, activation='relu', input_shape=[input_size,1]))
 	model.add(Dropout(.2))
 	model.add(Dense(256, activation='relu'))
 	model.add(Dropout(.2))
@@ -75,9 +76,11 @@ def neural_network_model(input_size):
 	model.add(Dropout(.2))
 	model.add(Dense(128, activation='relu'))
 	model.add(Dropout(.2))
-	model.add(Dense(2, activation='softmax'))
-	model.compile(optimizer='Adam', learning_rate=lr,
-				loss='categorical_crossentropy')
+	model.add(Flatten())
+	model.add(Dense(1, activation='sigmoid'))
+	model.compile(optimizer=keras.optimizers.Adam(lr=lr),
+				loss='binary_crossentropy', metrics=['accuracy'])
+	print(type(model))
 	return model
 
 #tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
@@ -88,6 +91,30 @@ def train_model(training_data, model=False):
 	if not model:
 		model = neural_network_model(input_size = len(X[0]))
 	model.fit(X, y, epochs=5, verbose=2)
+	return model
 
 training_data = initial_population()
 model = train_model(training_data)
+scores = []
+choices = []
+for each_game in range(10):
+	score = 0
+	game_memory = []
+	prev_obs = []
+	env.reset()
+	for _ in range(goal_steps):
+		env.render()
+		if len(prev_obs) == 0:
+			action = random.randrange(0,2)
+		else:
+			action = int(np.round(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])[0])
+		choices.append(action)
+		new_observation, reward, done, info = env.step(action)
+		prev_obs = new_observation
+		game_memory.append([new_observation, action])
+		score += reward
+		if done:
+			break
+	scores.append(score)
+print('Average score:', sum(scores)/len(scores))
+print('Choice 1: {}, Choice 2: {}'.format(choices.count(1)/len(choices), choices.count(0)/len(choices)))
